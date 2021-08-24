63ddf1a36e247c6852298aa623b57c847d8f410c
Hunter Lawson <hunterlaw9son@gmail.com>
2021-08-24 00:02:21 -0400

    update setup to include keywords, issues

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bassg',
-    version='1.1.3',
+    version='1.1.4',
     license='MIT',
     author='Hunter Lawson',
     author_email='hawson7@gmail.com',
-    description='bassg - Basic Agile Static Site Generator',
-    url='https://github.com/HunterLawson/bassg',
+    description='BASSG - Basic Agile Static Site Generator',
+    url='https://github.com/hunterlawson/bassg',
+    project_urls={
+        'Bug Tracker': 'https://github.com/hunterlawson/bassg/issues'
+    },
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
@@ -19,6 +22,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    keywords=['static site generator', 'bassg'],
     python_requires='>=3.6',
     py_modules=['bassg'],
     install_requires=['beautifulsoup4', 'click', 'markdown', 'Jinja2']
